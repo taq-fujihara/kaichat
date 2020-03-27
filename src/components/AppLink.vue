a3a8cfa23d86a8d86c0aa0da621505b50ea628ed
taq-fujihara <taq.fujihara@gmail.com>
2020-03-27 22:23:34 +0900

    visitedとかで色を変えないリンクコンポーネントを追加

---
+++ src/components/AppLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <a class="app-link" :href="href" v-on="$listeners">
+    <slot />
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    href: {
+      type: String,
+      default: '#',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.app-link {
+  &:link,
+  &:visited {
+    color: inherit;
+  }
+}
+</style>
