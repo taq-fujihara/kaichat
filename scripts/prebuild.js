5707453f1f62f3f3274c8e0fa71d52dd2e02bc7e
taq-fujihara <taq.fujihara@gmail.com>
2020-04-11 14:28:01 +0900

    Lint!!!

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -5,18 +5,19 @@ const path = require("path");
 
 if (process.env.NODE_ENV) {
   // Backup firebase-messaging-sw.js
-  const targetFile = "firebase-messaging-sw.js";
-  const targetPath = path.join("public", targetFile);
-  const backupPath = path.join("scripts", `${targetFile}.bk`);
-  fs.copyFileSync(targetPath, backupPath);
+  const targetFile = 'firebase-messaging-sw.js'
+  const targetPath = path.join('public', targetFile)
+  const backupPath = path.join('scripts', `${targetFile}.bk`)
+  fs.copyFileSync(targetPath, backupPath)
 
   // Replace {{KEY}} with entries of process.env
-  let content = fs.readFileSync(targetPath).toString();
-  const matches = content.match(/\{\{.+\}\}/g) || [];
+  let content = fs.readFileSync(targetPath).toString()
+  const matches = content.match(/\{\{[a-zA-Z0-9_]+\}\}/g) || []
   for (const match of matches) {
-    const key = match.replace("{{", "").replace("}}", "");
-    content = content.replace(match, process.env[key]);
+    const key = match.replace('{{', '').replace('}}', '')
+    console.log(match, key)
+    content = content.replace(match, process.env[key])
   }
 
-  fs.writeFileSync(targetPath, content);
+  fs.writeFileSync(targetPath, content)
 }
